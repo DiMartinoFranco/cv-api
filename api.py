d3a9f0a386f6f9c279b42e4d6cca31ec3ec075af
silvertw <silver-computacion@hotmail.com>
2020-11-21 11:16:55 -0300

    primer commit api

---
+++ api.py
@@ -0,0 +1,87 @@
+#API ES LA FORMA EN QUE 2 APPS SE COMUNICAN ENTRE SI
+#application programming interface
+
+#formas de conectarse:
+#SDK, O LIBRERIA, DIRECTAMENTE EN CODIGO
+#REST basado en http, la mas popular// el q vamos a utilizar
+#SOAP Y OTROS: se estan dejando de usar
+
+
+
+#REST: REPRESENTATIONAL STATE TRANSFER
+
+#CLIENTE SERVIDOR
+#SIN ESTADO: no se guardan datos de contexto en el servidor
+#CACHEABLE
+#SISTEMA EN CAPAS
+#CODIGO A DEMANDA:pueden recibir codigo ejecutable
+#INTERFAZ UNIFORME
+
+#!flask/bin/python
+# -*- coding: UTF-8 -*-
+
+from flask import Flask, request, jsonify, abort
+
+app= Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+
+@app.route('/')
+def index():
+    info={
+        "mensaje" : "Bienvenido a la API",
+        "acciones" : [
+            "GET /curriculum"
+            "POST /mensajes" 
+        ]
+    }
+    return jsonify(info)
+
+@app.route('/curriculum', methods=['GET'])
+def cv():
+    url_imagen = request.host_url + "static/amigosmtb.png"
+    cv = {
+        "nombre" : "Nahuel",
+        "apellido" : "Tori",
+        "residencia" : "Argentina",
+        "experiencia" : [{
+            "posicion" : "< describe tu posición>",
+            "empresa" : "< nombre de tu empresa >",
+            "desde" : "< cuándo empezaste a trabajar >",
+            "hasta" : "< si ya no trabajas más, cuándo >",
+            "descripcion" : "< detalles >"
+        }],
+        "educación" : {
+            "nivel" : "< nivel de tus estudios >",
+            "titulo" : "< nombre de tu carrera >",
+            "institucion" : "< dónde estudiaste >",
+            "facultad" : "< más detalles >"
+        },
+        "intereses" : ["python", "apis", "enseñar"],
+        "redes" : {
+            "github" : "https://github.com/nahueltori",
+            "twitter" : "https://twitter.com/nahueltori",
+            "linkedin" : "https://www.linkedin.com/in/nahueltori"
+        },
+        "foto" : url_imagen
+    }
+    return jsonify(cv)
+
+
+
+
+@app.route('/mensajes', methods=['POST'])
+def contacto():
+    mensaje = request.get_data()
+
+    if not mensaje:
+        abort(400, description="Debe enviar su mensaje en el body del POST.")
+    print("MENSAJE DE CONTACTO: " + str(mensaje))
+    return "Gracias por su mensaje."
+
+if __name__=='__main__':
+    app.run()
+
+
+
